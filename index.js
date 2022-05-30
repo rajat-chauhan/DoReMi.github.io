4eabd9f2dea397972ebf1c10b2fdd45ae5ff887e
rajat-chauhan <rajc87417@gmail.com>
2022-05-30 20:39:25 +0530

    update

--- index.js
+++ index.js
@@ -2,42 +2,57 @@ let mykeys = document.querySelectorAll(".key");
 
 mykeys.forEach(value => {
     value.addEventListener('click', function(){
-        playSound(value.textContent);
+        playSound(value.textContent.toLowerCase());
     })
 });
 
+document.addEventListener('keydown', function(btn){
+    playSound(btn.key.toLowerCase());
+
+    let pressed_key = btn.key.toLowerCase();
+
+    mykeys.forEach(value => {
+        value.classList.remove("activated");
+    })
+    
+    mykeys.forEach(value => {
+        if(pressed_key == value.innerHTML.toLowerCase()){
+           value.classList.add("activated"); 
+        }
+    })
+})
 
 function playSound(key) {
     switch (key) {
-        case 'C':
+        case 'c':
             var audio = new Audio('doremi/do.mp3');
             audio.play();
             break;
-        case 'D':
+        case 'd':
             var audio = new Audio('doremi/doh.mp3');
             audio.play();
             break;
-        case 'E':
+        case 'e':
             var audio = new Audio('doremi/fa.mp3');
             audio.play();
             break;
-        case 'F':
+        case 'f':
             var audio = new Audio('doremi/la.mp3');
             audio.play();
             break;
-        case 'G':
+        case 'g':
             var audio = new Audio('doremi/mi.mp3');
             audio.play();
             break;
-        case 'A':
+        case 'a':
             var audio = new Audio('doremi/re.mp3');
             audio.play();
             break;
-        case 'B':
+        case 'b':
             var audio = new Audio('doremi/sol.mp3');
             audio.play();
             break;
-        case 'K':
+        case 'k':
             var audio = new Audio('doremi/ti.mp3');
             audio.play();
             break;
